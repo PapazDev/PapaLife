b4bd9913273015e9d7d823a23cbae63642237644
kqq <971340511@qq.com>
2020-03-31 21:23:29 +0800

    日历操作

--- lib/pages/calender/calender.dart
+++ lib/pages/calender/calender.dart
@@ -1,5 +1,6 @@
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:device_calendar/device_calendar.dart';
 
 class MyCalenderPage extends StatefulWidget {
   MyCalenderPage({Key key, this.title}) : super(key: key);
@@ -50,6 +51,48 @@ class Meeting {
 }
 
 class _CalenderState extends State<MyCalenderPage> {
+  DeviceCalendarPlugin _deviceCalendarPlugin = new DeviceCalendarPlugin();
+
+  final _meetings = <Meeting>[];
+
+  void retrieveCalendars() async {
+    final DateTime today = DateTime.now();
+    final DateTime startTime =
+        DateTime(today.year, today.month, today.day, 9, 0, 0);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    //Retrieve user's calendars from mobile device
+    //Request permissions first if they haven't been granted
+    try {
+      var permissionsGranted = await _deviceCalendarPlugin.hasPermissions();
+      if (permissionsGranted.isSuccess && !permissionsGranted.data) {
+        permissionsGranted = await _deviceCalendarPlugin.requestPermissions();
+        if (!permissionsGranted.isSuccess || !permissionsGranted.data) {
+          return;
+        }
+      }
+
+      final calendarsResult = await _deviceCalendarPlugin.retrieveCalendars();
+      setState(() {
+        var list = _deviceCalendarPlugin.retrieveEvents(
+            calendarsResult?.data[0].id,
+            RetrieveEventsParams(
+                startDate: DateTime.parse('2019-12-11'),
+                endDate: DateTime.now()));
+        list.then((value) {
+          value.data.forEach((f) {
+            _meetings.add(Meeting(
+                f.title, f.start, f.end, const Color(0xFF0F8644), f.allDay));
+          });
+          //print(_meetings.length);
+          _meetings.add(Meeting('Conference', startTime, endTime,
+              const Color(0xFF0F8644), false));
+        });
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,13 +106,15 @@ class _CalenderState extends State<MyCalenderPage> {
   }
 
   List<Meeting> _getDataSource() {
-    var meetings = <Meeting>[];
+    var meetings = _meetings;
+    _meetings.clear();
     final DateTime today = DateTime.now();
     final DateTime startTime =
         DateTime(today.year, today.month, today.day, 9, 0, 0);
     final DateTime endTime = startTime.add(const Duration(hours: 2));
-    meetings.add(Meeting(
+    _meetings.add(Meeting(
         'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+    retrieveCalendars();
     return meetings;
   }
 }
