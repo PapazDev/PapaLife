eb404484c23172d3e9a8e4f600f613e046773027
kqq <971340511@qq.com>
2020-03-31 17:34:20 +0800

    计时器实现

--- lib/pages/index/index.dart
+++ lib/pages/index/index.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -19,6 +21,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Map> todoList = [
+    {'name': '阅读', 'id': '123', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写1代码', 'id': '312131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0},
+    {'name': '写代码', 'id': '32131231', 'time': 0}
+  ];
+  void startTimer(index) {
+    const oneSec = const Duration(microseconds: 10);
+    todoList[index]['timer'] = new Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+        () {
+          print(timer);
+          todoList[index]['time']= todoList[index]['time']+1;
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -32,6 +61,9 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisSize: MainAxisSize.min,
               // mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
+                /**
+                 * 日程
+                 */
                 new Card(
                   elevation: 0, //设置阴影
                   shape: const RoundedRectangleBorder(
@@ -41,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     // card只能有一个widget，但这个widget内容可以包含其他的widget
                     children: [
                       new ListTile(
-                        title: new Text('待做',
+                        title: new Text('日程',
                             style: new TextStyle(fontWeight: FontWeight.w500)),
                         leading: new Icon(
                           Icons.restaurant_menu,
@@ -50,15 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
                         trailing: Icon(Icons.more_vert),
                       ),
                       new Divider(),
-                      new ListTile(
-                        title: new Text('内容一',
-                            style: new TextStyle(fontWeight: FontWeight.w500)),
-                        leading: new Icon(
-                          Icons.contact_phone,
-                          color: Colors.blue[500],
-                        ),
-                        trailing: Icon(Icons.check_circle_outline),
-                      ),
                       new Dismissible(
                         key: Key('1'),
                         background: Container(
@@ -92,41 +115,97 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                 ),
+
+                /**
+                 * 待做
+                 */
                 new Card(
-                  elevation: 0, //设置阴影
-                  shape: const RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.all(Radius.circular(14.0))), //设置圆角
-                  child: new Column(
-                    // card只能有一个widget，但这个widget内容可以包含其他的widget
-                    children: [
-                      new ListTile(
-                        title: new Text('打卡',
-                            style: new TextStyle(fontWeight: FontWeight.w500)),
-                        leading: new Icon(
-                          Icons.restaurant_menu,
-                          color: Colors.blue[500],
+                    elevation: 0, //设置阴影
+                    shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.all(Radius.circular(14.0))), //设置圆角
+                    child: new Column(
+                      children: <Widget>[
+                        new ListTile(
+                          title: new Text('待做',
+                              style:
+                                  new TextStyle(fontWeight: FontWeight.w500)),
+                          leading: new Icon(
+                            Icons.restaurant_menu,
+                            color: Colors.blue[500],
+                          ),
+                          trailing: Icon(Icons.more_vert),
                         ),
-                      ),
-                      new Divider(),
-                      new ListTile(
-                        title: new Text('内容一',
-                            style: new TextStyle(fontWeight: FontWeight.w500)),
-                        leading: new Icon(
-                          Icons.contact_phone,
-                          color: Colors.blue[500],
-                        ),
-                      ),
-                      new ListTile(
-                        title: new Text('内容二'),
-                        leading: new Icon(
-                          Icons.contact_mail,
-                          color: Colors.blue[500],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                        new Divider(),
+                        ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: todoList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Dismissible(
+                                key: Key(todoList[index]['id']),
+                                background: Container(
+                                  color: Colors.red,
+                                  alignment: Alignment.centerLeft,
+                                  child: Icon(Icons.delete),
+                                ),
+                                secondaryBackground: Container(
+                                  color: Colors.blue,
+                                  alignment: AlignmentDirectional.centerEnd,
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        0.0, 0.0, 10.0, 0.0),
+                                    child: Icon(
+                                      Icons.delete,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                child: new ListTile(
+                                  title: new Text(todoList[index]['name']),
+                                  subtitle: Text(new DateFormat('HH:mm:ss:SSS')
+                                      .format(
+                                          DateTime.fromMillisecondsSinceEpoch(
+                                              todoList[index]['time']))),
+                                  leading: new Icon(
+                                    Icons.contact_mail,
+                                    color: Colors.blue[500],
+                                  ),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      IconButton(
+                                        icon: Icon(Icons.play_arrow),
+                                        onPressed: () {
+                                          print('strat');
+                                          startTimer(index);
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: Icon(Icons.pause),
+                                        onPressed: () {
+                                          print('end');
+                                          todoList[index]['timer'].cancel();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                  onTap: () {
+                                    print(todoList[index]['id']);
+                                  },
+                                ),
+                                confirmDismiss: (direction) async {
+                                  print(direction);
+                                  return false;
+                                },
+                              );
+                            }),
+                      ],
+                    )),
+
+                /**
+                 * 记账
+                 */
                 new Card(
                   elevation: 0, //设置阴影
                   shape: const RoundedRectangleBorder(
