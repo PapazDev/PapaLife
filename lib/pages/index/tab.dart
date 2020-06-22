2565cc106e89b712f8d097bc16b1b25028b20401
Zrxrxrx <Zrx23333@gmail.com>
2020-06-22 21:33:41 +1000

    fix: tab.dart indent

--- lib/pages/index/tab.dart
+++ lib/pages/index/tab.dart
@@ -21,75 +21,74 @@ class _TabsState extends State<Tabs> {
     {"title": '日历', "page": MyCalenderPage()},
   ];
   final _scaffoldkey = GlobalKey<ScaffoldState>();
-  @override
   Widget build(BuildContext context) {
     return Container(
         child: Scaffold(
-      key: _scaffoldkey,
-      // 顶部BAR
-      appBar: AppBar(
-        title: Text(this._pagesList[this._currentIndex]['title'],
-            style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: null,
-            color: Colors.black,
+          key: _scaffoldkey,
+          // 顶部BAR
+          appBar: AppBar(
+            title: Text(this._pagesList[this._currentIndex]['title'],
+                style: TextStyle(color: Colors.black)),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: null,
+                color: Colors.black,
+              ),
+            ],
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              color: Colors.black,
+              onPressed: () => _scaffoldkey.currentState.openDrawer(),
+            ),
           ),
-        ],
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          color: Colors.black,
-          onPressed: () => _scaffoldkey.currentState.openDrawer(),
-        ),
-      ),
-      //主页面
-      body: this._pagesList[this._currentIndex]["page"],
-      //左边菜单
-      drawer: Menu.homeDrawer(),
-      // 底部导航栏
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: this._currentIndex,
-          onTap: (index) {
-            this.setState(() {
-              this._currentIndex = index;
-            });
-          },
-          type: BottomNavigationBarType.fixed,
-          fixedColor: Colors.red,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('首页'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today),
-              title: Text('日历'),
-            ),
-          ]),
-      // 加号按钮
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(new PageRouteBuilder(pageBuilder:
-              (BuildContext context, Animation<double> animation,
+          //主页面
+          body: this._pagesList[this._currentIndex]["page"],
+          //左边菜单
+          drawer: Menu.homeDrawer(),
+          // 底部导航栏
+          bottomNavigationBar: BottomNavigationBar(
+              currentIndex: this._currentIndex,
+              onTap: (index) {
+                this.setState(() {
+                  this._currentIndex = index;
+                });
+              },
+              type: BottomNavigationBarType.fixed,
+              fixedColor: Colors.red,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Text('首页'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_today),
+                  title: Text('日历'),
+                ),
+              ]),
+          // 加号按钮
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(new PageRouteBuilder(pageBuilder:
+                  (BuildContext context, Animation<double> animation,
                   Animation<double> secondaryAnimation) {
-            return new Add();
-          }, transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) {
-            return PapaAnimation.createTransition(animation, child);
-          }));
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    ));
+                return new Add();
+              }, transitionsBuilder: (
+                  BuildContext context,
+                  Animation<double> animation,
+                  Animation<double> secondaryAnimation,
+                  Widget child,
+                  ) {
+                return PapaAnimation.createTransition(animation, child);
+              }));
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        ));
   }
 }
